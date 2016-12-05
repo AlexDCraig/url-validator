8d724b8e926e43777e9f9ec88833e980e5533cb8
Brandon Chatham <chathamb@flip3.engr.oregonstate.edu>
2016-12-04 18:03:57 -0800

    Removed a directory.

---
+++ src/test/java/osu/cs362/URLValidator/testDomainValidator.java
@@ -0,0 +1,23 @@
+package osu.cs362.URLValidator;
+
+import java.io.Serializable;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+public class testDomainValidator
+{
+	@Test
+	public void testCaseSensitive()
+	{
+		InetAddressValidator addValidator = new InetAddressValidator();
+	}
+
+	@Test
+	public void testRegexSizeValidation()
+	{
+		int space = 5;
+	}
+}
